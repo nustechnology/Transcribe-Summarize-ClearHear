984d16668da13d88e416e15bd83410150d337d0e
thangn <thangn@nustechnology.com>
2026-07-01 09:29:20 +0700

    Fixed some new bugs.

--- lib/lang/string_keys.dart
+++ lib/lang/string_keys.dart
@@ -1,5 +1,9 @@
-/// Translation key constants. Use with GetX: [StringKeys.appTitle].tr
-abstract final class StringKeys {
+import 'package:get/get.dart';
+
+/// Translation key constants. Use with GetX: `StringKeys.appTitle.tr`
+abstract class StringKeys {
+  StringKeys._();
+
   static const appTitle = 'app_title';
   static const homeStatusIdle = 'home_status_idle';
   static const homeStatusActive = 'home_status_active';
@@ -9,4 +13,24 @@ abstract final class StringKeys {
   static const homeStartCaptioning = 'home_start_captioning';
   static const homeStopCaptioning = 'home_stop_captioning';
   static const homeStop = 'home_stop';
+  static const homeListening = 'home_listening';
+  static const homeProcessing = 'home_processing';
+  static const homeSummaryLabel = 'home_summary_label';
+  static const microphonePermissionDenied = 'microphone_permission_denied';
+  static const transcriptionFailed = 'transcription_failed';
+  static const summaryFailed = 'summary_failed';
+  static const summaryModelFailed = 'summary_model_failed';
+  static const recorderUnavailable = 'recorder_unavailable';
+  static const navLive = 'nav_live';
+  static const navHistory = 'nav_history';
+  static const navSettings = 'nav_settings';
+  static const homeLanguageEnglish = 'home_language_english';
+  static const homeConfidenceLabel = 'home_confidence_label';
+  static const homeConfidenceMedium = 'home_confidence_medium';
+  static const homePrivacyNote = 'home_privacy_note';
+  static const homeFontDecrease = 'home_font_decrease';
+  static const homeFontIncrease = 'home_font_increase';
+  static const homeSummarize = 'home_summarize';
+
+  static String t(String key) => key.tr;
 }
