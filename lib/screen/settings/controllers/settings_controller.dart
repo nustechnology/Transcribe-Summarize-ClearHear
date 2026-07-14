314eb2166e335fde80dd7b12dd1740781c0e39dc
nusteam <github@nustechnology.com>
2026-07-14 11:07:40 +0700

    fix(settings): show the success banner after clearing all data (#23)

--- lib/screen/settings/controllers/settings_controller.dart
+++ lib/screen/settings/controllers/settings_controller.dart
@@ -138,7 +138,7 @@ class SettingsController extends GetxController {
         await Get.find<HistoryController>().loadHistory();
       }
 
-      AppToast.success(StringKeys.settingsClearDataSuccess.tr);
+      AppToast.sessionDeleted(title: StringKeys.settingsClearDataSuccess.tr);
     } catch (e) {
       AppLogger.error(error: e);
       AppToast.error(StringKeys.somethingWentWrong.tr);
